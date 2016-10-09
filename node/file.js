71219ab852e6f65c483556a583e6fd6747aa93a6
pix <tpxsky@163.com>
2016-10-09 17:08:01 +0800

    first commit

---
+++ node/file.js
@@ -0,0 +1,40 @@
+var optional = require("optional");
+
+var FS = require("fs");
+
+var C = console;
+
+var MIME = require("MIME");
+var CONFIG = require("../js/config.js");
+var ERRORS = require("./errors.js");
+
+module.exports = {
+    
+    /*
+    canServe: function(req, res, info) {
+        return true;
+    }, */
+    
+    serve: function file(req, res, info) {
+        var file = CONFIG.WEB + info.path;
+        FS.readFile(file, function(error, data) {
+            if (error) {
+                C.error(info + "[ERROR] can not read file: " + file + " error: " + error);
+                ERRORS["404"](req, res, info);
+            }
+            
+            var headers = {};
+            var dot = ".";
+            var lastIndex = file.lastIndexOf(dot);
+            if (lastIndex >= 0) {
+                var fileExtension = file.substring(lastIndex + dot.length);
+                headers["Content-Type"] = MIME[fileExtension];
+                C.log(info + "res: " + file + " [" + (MIME[fileExtension] || "unknown") + "]");
+            }
+            res.writeHead(200, headers);
+            
+            res.end(data);
+        });
+    }
+};
+
